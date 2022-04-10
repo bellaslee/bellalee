ff6b6c5f605108585791e181195f6d5463c72d9a
bella <bella@bellalee.com>
2022-04-10 15:08:04 -0700

    small update

---
+++ src/Components/ScrollIndicator.js
@@ -0,0 +1,12 @@
+import '../assets/css/ScrollIndicator.css';
+import React from "react";
+
+function ScrollIndicator() {
+  return (
+    <div className="scroll-indicator">
+      <span class="material-icons arrow-down">keyboard_arrow_down</span>
+    </div>
+  )
+}
+
+export default ScrollIndicator;
