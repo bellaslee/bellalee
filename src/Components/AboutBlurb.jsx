b6ea5fba2b1414db33741ef32539f6a2bb6879ad
bella <bella@bellalee.com>
2022-05-21 22:53:47 -0700

    updated about blurb, fixed link styling issues

--- src/Components/AboutBlurb.jsx
+++ src/Components/AboutBlurb.jsx
@@ -6,25 +6,26 @@ export default function AboutBlurb() {
       <h2>Hi! I'm Bella.</h2>
       <p>
         I am a first-year student at the University of Washington intending on
-        majoring in Visual Communication Design and Informatics. I started out
-        100% sure I wanted to be a graphic designer, but I unexpectedly fell in
-        love with web programming, too. Now, my dream is to work in the
-        intersection between visual design and technology. If I’m able to make
-        even one person’s more delightful through my creations, then I can be
-        satisfied.
+        majoring in Visual Communication Design and Informatics. My dream is to
+        work in the intersection between visual communication and technology;
+        lately, I've become very interested in creative coding. I hope to make
+        information more accessible, intuitive, and enjoyable to consume through
+        my work.
       </p>
       <p>
         I am the Chief Design Officer for Smirk UW and am currently working on
-        Design and Web Development for the Stoma Project in the Sensors, Energy,
-        and Automation Laboratory.
+        Design and Web Development in the{' '}
+        <a href="https://www.uwseal.org/" target="_blank" rel="noreferrer">
+          Sensors, Energy, and Automation Laboratory
+        </a>
+        .
       </p>
       <p>
         In my own time, I like to create illustrations of my favorite
-        characters, record covers of my favorite songs, and dance in my room as
-        I play my favorite playlist through my Bluetooth speaker. If I’m not on
-        campus, you can most definitely find me at my favorite restaurant,
-        Meesum Pastry, indulging in the nostalgic and comforting taste of
-        Taiwanese cuisine.
+        characters, play fingerstyle guitar, and get engrossed in my latest
+        interest (right now, I want to learn p5.js). If I’m not on campus, you
+        can most likely find me at my favorite restaurant, Meesum Pastry,
+        indulging in the nostalgic and comforting taste of Taiwanese cuisine.
       </p>
     </React.Fragment>
   );
